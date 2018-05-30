0fb19771d468916ff95ea3e0bbc34f92aff7f60c
uerfan <844186956@qq.com>
2018-05-30 19:42:32 +0800

    wd

---
+++ hw/block/ox-ctrl/mmgr/volt/bch.d
@@ -0,0 +1,4 @@
+hw/block/ox-ctrl/mmgr/volt/bch.o: hw/block/ox-ctrl/mmgr/volt/bch.c \
+ hw/block/ox-ctrl/mmgr/volt/bch.h
+
+hw/block/ox-ctrl/mmgr/volt/bch.h:
---
+++ hw/scsi/scsi-bus.d
@@ -0,0 +1,469 @@
+hw/scsi/scsi-bus.o: hw/scsi/scsi-bus.c \
+ /home/lf/qemu-ox/include/qemu/osdep.h config-host.h \
+ /home/lf/qemu-ox/include/exec/poison.h \
+ /home/lf/qemu-ox/include/qemu/compiler.h \
+ /home/lf/qemu-ox/include/sysemu/os-posix.h \
+ /home/lf/qemu-ox/include/glib-compat.h /usr/include/glib-2.0/glib.h \
+ /usr/include/glib-2.0/glib/galloca.h /usr/include/glib-2.0/glib/gtypes.h \
+ /usr/lib64/glib-2.0/include/glibconfig.h \
+ /usr/include/glib-2.0/glib/gmacros.h \
+ /usr/include/glib-2.0/glib/gversionmacros.h \
+ /usr/include/glib-2.0/glib/garray.h \
+ /usr/include/glib-2.0/glib/gasyncqueue.h \
+ /usr/include/glib-2.0/glib/gthread.h \
+ /usr/include/glib-2.0/glib/gatomic.h /usr/include/glib-2.0/glib/gerror.h \
+ /usr/include/glib-2.0/glib/gquark.h /usr/include/glib-2.0/glib/gutils.h \
+ /usr/include/glib-2.0/glib/gbacktrace.h \
+ /usr/include/glib-2.0/glib/gbase64.h \
+ /usr/include/glib-2.0/glib/gbitlock.h \
+ /usr/include/glib-2.0/glib/gbookmarkfile.h \
+ /usr/include/glib-2.0/glib/gbytes.h \
+ /usr/include/glib-2.0/glib/gcharset.h \
+ /usr/include/glib-2.0/glib/gchecksum.h \
+ /usr/include/glib-2.0/glib/gconvert.h \
+ /usr/include/glib-2.0/glib/gdataset.h /usr/include/glib-2.0/glib/gdate.h \
+ /usr/include/glib-2.0/glib/gdatetime.h \
+ /usr/include/glib-2.0/glib/gtimezone.h /usr/include/glib-2.0/glib/gdir.h \
+ /usr/include/glib-2.0/glib/genviron.h \
+ /usr/include/glib-2.0/glib/gfileutils.h \
+ /usr/include/glib-2.0/glib/ggettext.h /usr/include/glib-2.0/glib/ghash.h \
+ /usr/include/glib-2.0/glib/glist.h /usr/include/glib-2.0/glib/gmem.h \
+ /usr/include/glib-2.0/glib/gnode.h /usr/include/glib-2.0/glib/ghmac.h \
+ /usr/include/glib-2.0/glib/gchecksum.h \
+ /usr/include/glib-2.0/glib/ghook.h \
+ /usr/include/glib-2.0/glib/ghostutils.h \
+ /usr/include/glib-2.0/glib/giochannel.h \
+ /usr/include/glib-2.0/glib/gmain.h /usr/include/glib-2.0/glib/gpoll.h \
+ /usr/include/glib-2.0/glib/gslist.h /usr/include/glib-2.0/glib/gstring.h \
+ /usr/include/glib-2.0/glib/gunicode.h \
+ /usr/include/glib-2.0/glib/gkeyfile.h \
+ /usr/include/glib-2.0/glib/gmappedfile.h \
+ /usr/include/glib-2.0/glib/gmarkup.h \
+ /usr/include/glib-2.0/glib/gmessages.h \
+ /usr/include/glib-2.0/glib/goption.h \
+ /usr/include/glib-2.0/glib/gpattern.h \
+ /usr/include/glib-2.0/glib/gprimes.h /usr/include/glib-2.0/glib/gqsort.h \
+ /usr/include/glib-2.0/glib/gqueue.h /usr/include/glib-2.0/glib/grand.h \
+ /usr/include/glib-2.0/glib/gregex.h \
+ /usr/include/glib-2.0/glib/gscanner.h \
+ /usr/include/glib-2.0/glib/gsequence.h \
+ /usr/include/glib-2.0/glib/gshell.h /usr/include/glib-2.0/glib/gslice.h \
+ /usr/include/glib-2.0/glib/gspawn.h \
+ /usr/include/glib-2.0/glib/gstrfuncs.h \
+ /usr/include/glib-2.0/glib/gstringchunk.h \
+ /usr/include/glib-2.0/glib/gtestutils.h \
+ /usr/include/glib-2.0/glib/gthreadpool.h \
+ /usr/include/glib-2.0/glib/gtimer.h \
+ /usr/include/glib-2.0/glib/gtrashstack.h \
+ /usr/include/glib-2.0/glib/gtree.h \
+ /usr/include/glib-2.0/glib/gurifuncs.h \
+ /usr/include/glib-2.0/glib/gvarianttype.h \
+ /usr/include/glib-2.0/glib/gvariant.h \
+ /usr/include/glib-2.0/glib/gversion.h \
+ /usr/include/glib-2.0/glib/deprecated/gallocator.h \
+ /usr/include/glib-2.0/glib/deprecated/gcache.h \
+ /usr/include/glib-2.0/glib/deprecated/gcompletion.h \
+ /usr/include/glib-2.0/glib/deprecated/gmain.h \
+ /usr/include/glib-2.0/glib/deprecated/grel.h \
+ /usr/include/glib-2.0/glib/deprecated/gthread.h \
+ /usr/include/glib-2.0/glib/glib-autocleanups.h \
+ /home/lf/qemu-ox/include/qemu/typedefs.h \
+ /home/lf/qemu-ox/include/hw/hw.h \
+ /home/lf/qemu-ox/include/exec/cpu-common.h \
+ /home/lf/qemu-ox/include/exec/hwaddr.h \
+ /home/lf/qemu-ox/include/qemu/bswap.h \
+ /home/lf/qemu-ox/include/fpu/softfloat.h \
+ /home/lf/qemu-ox/include/qemu/queue.h \
+ /home/lf/qemu-ox/include/qemu/atomic.h \
+ /home/lf/qemu-ox/include/qemu/fprintf-fn.h \
+ /home/lf/qemu-ox/include/qom/object.h qapi-types.h \
+ /home/lf/qemu-ox/include/exec/memory.h \
+ /home/lf/qemu-ox/include/exec/memattrs.h \
+ /home/lf/qemu-ox/include/qemu/int128.h \
+ /home/lf/qemu-ox/include/qemu/notify.h \
+ /home/lf/qemu-ox/include/qemu/rcu.h \
+ /home/lf/qemu-ox/include/qemu/thread.h \
+ /home/lf/qemu-ox/include/qemu/processor.h \
+ /home/lf/qemu-ox/include/qemu/thread-posix.h \
+ /home/lf/qemu-ox/include/hw/irq.h \
+ /home/lf/qemu-ox/include/migration/vmstate.h \
+ /home/lf/qemu-ox/include/migration/qemu-file.h \
+ /home/lf/qemu-ox/include/qemu-common.h \
+ /home/lf/qemu-ox/include/qemu/fprintf-fn.h \
+ /home/lf/qemu-ox/include/qemu/option.h \
+ /home/lf/qemu-ox/include/qapi/qmp/qdict.h \
+ /home/lf/qemu-ox/include/qapi/qmp/qobject.h \
+ /home/lf/qemu-ox/include/qapi/qmp/qlist.h \
+ /home/lf/qemu-ox/include/qemu/module.h \
+ /home/lf/qemu-ox/include/io/channel.h \
+ /home/lf/qemu-ox/include/migration/qjson.h \
+ /home/lf/qemu-ox/include/qemu/module.h \
+ /home/lf/qemu-ox/include/qapi/error.h \
+ /home/lf/qemu-ox/include/qemu/error-report.h \
+ /home/lf/qemu-ox/include/hw/scsi/scsi.h \
+ /home/lf/qemu-ox/include/hw/qdev.h \
+ /home/lf/qemu-ox/include/hw/qdev-core.h \
+ /home/lf/qemu-ox/include/qemu/option.h \
+ /home/lf/qemu-ox/include/qemu/bitmap.h \
+ /home/lf/qemu-ox/include/qemu/bitops.h \
+ /home/lf/qemu-ox/include/qemu/host-utils.h \
+ /home/lf/qemu-ox/include/qemu/atomic.h \
+ /home/lf/qemu-ox/include/hw/hotplug.h \
+ /home/lf/qemu-ox/include/hw/qdev-properties.h \
+ /home/lf/qemu-ox/include/hw/block/block.h \
+ /home/lf/qemu-ox/include/sysemu/sysemu.h \
+ /home/lf/qemu-ox/include/qemu/timer.h \
+ /home/lf/qemu-ox/include/qemu/host-utils.h \
+ /home/lf/qemu-ox/include/sysemu/cpus.h \
+ /home/lf/qemu-ox/include/qemu/main-loop.h \
+ /home/lf/qemu-ox/include/block/aio.h \
+ /home/lf/qemu-ox/include/qemu/event_notifier.h \
+ /home/lf/qemu-ox/include/qemu/rfifolock.h \
+ /home/lf/qemu-ox/include/block/scsi.h \
+ /home/lf/qemu-ox/include/sysemu/block-backend.h \
+ /home/lf/qemu-ox/include/qemu/iov.h \
+ /home/lf/qemu-ox/include/block/throttle-groups.h \
+ /home/lf/qemu-ox/include/qemu/throttle.h \
+ /home/lf/qemu-ox/include/block/block_int.h \
+ /home/lf/qemu-ox/include/block/accounting.h \
+ /home/lf/qemu-ox/include/qemu/timed-average.h \
+ /home/lf/qemu-ox/include/block/block.h \
+ /home/lf/qemu-ox/include/qemu/coroutine.h \
+ /home/lf/qemu-ox/include/block/dirty-bitmap.h \
+ /home/lf/qemu-ox/include/qemu/hbitmap.h \
+ /home/lf/qemu-ox/include/qemu/bitops.h \
+ /home/lf/qemu-ox/include/block/snapshot.h \
+ /home/lf/qemu-ox/include/sysemu/blockdev.h \
+ /home/lf/qemu-ox/include/trace.h trace/generated-tracers.h \
+ trace/control.h trace/generated-events.h trace/event-internal.h \
+ trace/control-internal.h /home/lf/qemu-ox/include/qom/cpu.h \
+ /home/lf/qemu-ox/include/disas/bfd.h /home/lf/qemu-ox/include/qemu/log.h \
+ /home/lf/qemu-ox/include/sysemu/dma.h \
+ /home/lf/qemu-ox/include/exec/address-spaces.h \
+ /home/lf/qemu-ox/include/qemu/cutils.h
+
+/home/lf/qemu-ox/include/qemu/osdep.h:
+
+config-host.h:
+
+/home/lf/qemu-ox/include/exec/poison.h:
+
+/home/lf/qemu-ox/include/qemu/compiler.h:
+
+/home/lf/qemu-ox/include/sysemu/os-posix.h:
+
+/home/lf/qemu-ox/include/glib-compat.h:
+
+/usr/include/glib-2.0/glib.h:
+
+/usr/include/glib-2.0/glib/galloca.h:
+
+/usr/include/glib-2.0/glib/gtypes.h:
+
+/usr/lib64/glib-2.0/include/glibconfig.h:
+
+/usr/include/glib-2.0/glib/gmacros.h:
+
+/usr/include/glib-2.0/glib/gversionmacros.h:
+
+/usr/include/glib-2.0/glib/garray.h:
+
+/usr/include/glib-2.0/glib/gasyncqueue.h:
+
+/usr/include/glib-2.0/glib/gthread.h:
+
+/usr/include/glib-2.0/glib/gatomic.h:
+
+/usr/include/glib-2.0/glib/gerror.h:
+
+/usr/include/glib-2.0/glib/gquark.h:
+
+/usr/include/glib-2.0/glib/gutils.h:
+
+/usr/include/glib-2.0/glib/gbacktrace.h:
+
+/usr/include/glib-2.0/glib/gbase64.h:
+
+/usr/include/glib-2.0/glib/gbitlock.h:
+
+/usr/include/glib-2.0/glib/gbookmarkfile.h:
+
+/usr/include/glib-2.0/glib/gbytes.h:
+
+/usr/include/glib-2.0/glib/gcharset.h:
+
+/usr/include/glib-2.0/glib/gchecksum.h:
+
+/usr/include/glib-2.0/glib/gconvert.h:
+
+/usr/include/glib-2.0/glib/gdataset.h:
+
+/usr/include/glib-2.0/glib/gdate.h:
+
+/usr/include/glib-2.0/glib/gdatetime.h:
+
+/usr/include/glib-2.0/glib/gtimezone.h:
+
+/usr/include/glib-2.0/glib/gdir.h:
+
+/usr/include/glib-2.0/glib/genviron.h:
+
+/usr/include/glib-2.0/glib/gfileutils.h:
+
+/usr/include/glib-2.0/glib/ggettext.h:
+
+/usr/include/glib-2.0/glib/ghash.h:
+
+/usr/include/glib-2.0/glib/glist.h:
+
+/usr/include/glib-2.0/glib/gmem.h:
+
+/usr/include/glib-2.0/glib/gnode.h:
+
+/usr/include/glib-2.0/glib/ghmac.h:
+
+/usr/include/glib-2.0/glib/gchecksum.h:
+
+/usr/include/glib-2.0/glib/ghook.h:
+
+/usr/include/glib-2.0/glib/ghostutils.h:
+
+/usr/include/glib-2.0/glib/giochannel.h:
+
+/usr/include/glib-2.0/glib/gmain.h:
+
+/usr/include/glib-2.0/glib/gpoll.h:
+
+/usr/include/glib-2.0/glib/gslist.h:
+
+/usr/include/glib-2.0/glib/gstring.h:
+
+/usr/include/glib-2.0/glib/gunicode.h:
+
+/usr/include/glib-2.0/glib/gkeyfile.h:
+
+/usr/include/glib-2.0/glib/gmappedfile.h:
+
+/usr/include/glib-2.0/glib/gmarkup.h:
+
+/usr/include/glib-2.0/glib/gmessages.h:
+
+/usr/include/glib-2.0/glib/goption.h:
+
+/usr/include/glib-2.0/glib/gpattern.h:
+
+/usr/include/glib-2.0/glib/gprimes.h:
+
+/usr/include/glib-2.0/glib/gqsort.h:
+
+/usr/include/glib-2.0/glib/gqueue.h:
+
+/usr/include/glib-2.0/glib/grand.h:
+
+/usr/include/glib-2.0/glib/gregex.h:
+
+/usr/include/glib-2.0/glib/gscanner.h:
+
+/usr/include/glib-2.0/glib/gsequence.h:
+
+/usr/include/glib-2.0/glib/gshell.h:
+
+/usr/include/glib-2.0/glib/gslice.h:
+
+/usr/include/glib-2.0/glib/gspawn.h:
+
+/usr/include/glib-2.0/glib/gstrfuncs.h:
+
+/usr/include/glib-2.0/glib/gstringchunk.h:
+
+/usr/include/glib-2.0/glib/gtestutils.h:
+
+/usr/include/glib-2.0/glib/gthreadpool.h:
+
+/usr/include/glib-2.0/glib/gtimer.h:
+
+/usr/include/glib-2.0/glib/gtrashstack.h:
+
+/usr/include/glib-2.0/glib/gtree.h:
+
+/usr/include/glib-2.0/glib/gurifuncs.h:
+
+/usr/include/glib-2.0/glib/gvarianttype.h:
+
+/usr/include/glib-2.0/glib/gvariant.h:
+
+/usr/include/glib-2.0/glib/gversion.h:
+
+/usr/include/glib-2.0/glib/deprecated/gallocator.h:
+
+/usr/include/glib-2.0/glib/deprecated/gcache.h:
+
+/usr/include/glib-2.0/glib/deprecated/gcompletion.h:
+
+/usr/include/glib-2.0/glib/deprecated/gmain.h:
+
+/usr/include/glib-2.0/glib/deprecated/grel.h:
+
+/usr/include/glib-2.0/glib/deprecated/gthread.h:
+
+/usr/include/glib-2.0/glib/glib-autocleanups.h:
+
+/home/lf/qemu-ox/include/qemu/typedefs.h:
+
+/home/lf/qemu-ox/include/hw/hw.h:
+
+/home/lf/qemu-ox/include/exec/cpu-common.h:
+
+/home/lf/qemu-ox/include/exec/hwaddr.h:
+
+/home/lf/qemu-ox/include/qemu/bswap.h:
+
+/home/lf/qemu-ox/include/fpu/softfloat.h:
+
+/home/lf/qemu-ox/include/qemu/queue.h:
+
+/home/lf/qemu-ox/include/qemu/atomic.h:
+
+/home/lf/qemu-ox/include/qemu/fprintf-fn.h:
+
+/home/lf/qemu-ox/include/qom/object.h:
+
+qapi-types.h:
+
+/home/lf/qemu-ox/include/exec/memory.h:
+
+/home/lf/qemu-ox/include/exec/memattrs.h:
+
+/home/lf/qemu-ox/include/qemu/int128.h:
+
+/home/lf/qemu-ox/include/qemu/notify.h:
+
+/home/lf/qemu-ox/include/qemu/rcu.h:
+
+/home/lf/qemu-ox/include/qemu/thread.h:
+
+/home/lf/qemu-ox/include/qemu/processor.h:
+
+/home/lf/qemu-ox/include/qemu/thread-posix.h:
+
+/home/lf/qemu-ox/include/hw/irq.h:
+
+/home/lf/qemu-ox/include/migration/vmstate.h:
+
+/home/lf/qemu-ox/include/migration/qemu-file.h:
+
+/home/lf/qemu-ox/include/qemu-common.h:
+
+/home/lf/qemu-ox/include/qemu/fprintf-fn.h:
+
+/home/lf/qemu-ox/include/qemu/option.h:
+
+/home/lf/qemu-ox/include/qapi/qmp/qdict.h:
+
+/home/lf/qemu-ox/include/qapi/qmp/qobject.h:
+
+/home/lf/qemu-ox/include/qapi/qmp/qlist.h:
+
+/home/lf/qemu-ox/include/qemu/module.h:
+
+/home/lf/qemu-ox/include/io/channel.h:
+
+/home/lf/qemu-ox/include/migration/qjson.h:
+
+/home/lf/qemu-ox/include/qemu/module.h:
+
+/home/lf/qemu-ox/include/qapi/error.h:
+
+/home/lf/qemu-ox/include/qemu/error-report.h:
+
+/home/lf/qemu-ox/include/hw/scsi/scsi.h:
+
+/home/lf/qemu-ox/include/hw/qdev.h:
+
+/home/lf/qemu-ox/include/hw/qdev-core.h:
+
+/home/lf/qemu-ox/include/qemu/option.h:
+
+/home/lf/qemu-ox/include/qemu/bitmap.h:
+
+/home/lf/qemu-ox/include/qemu/bitops.h:
+
+/home/lf/qemu-ox/include/qemu/host-utils.h:
+
+/home/lf/qemu-ox/include/qemu/atomic.h:
+
+/home/lf/qemu-ox/include/hw/hotplug.h:
+
+/home/lf/qemu-ox/include/hw/qdev-properties.h:
+
+/home/lf/qemu-ox/include/hw/block/block.h:
+
+/home/lf/qemu-ox/include/sysemu/sysemu.h:
+
+/home/lf/qemu-ox/include/qemu/timer.h:
+
+/home/lf/qemu-ox/include/qemu/host-utils.h:
+
+/home/lf/qemu-ox/include/sysemu/cpus.h:
+
+/home/lf/qemu-ox/include/qemu/main-loop.h:
+
+/home/lf/qemu-ox/include/block/aio.h:
+
+/home/lf/qemu-ox/include/qemu/event_notifier.h:
+
+/home/lf/qemu-ox/include/qemu/rfifolock.h:
+
+/home/lf/qemu-ox/include/block/scsi.h:
+
+/home/lf/qemu-ox/include/sysemu/block-backend.h:
+
+/home/lf/qemu-ox/include/qemu/iov.h:
+
+/home/lf/qemu-ox/include/block/throttle-groups.h:
+
+/home/lf/qemu-ox/include/qemu/throttle.h:
+
+/home/lf/qemu-ox/include/block/block_int.h:
+
+/home/lf/qemu-ox/include/block/accounting.h:
+
+/home/lf/qemu-ox/include/qemu/timed-average.h:
+
+/home/lf/qemu-ox/include/block/block.h:
+
+/home/lf/qemu-ox/include/qemu/coroutine.h:
+
+/home/lf/qemu-ox/include/block/dirty-bitmap.h:
+
+/home/lf/qemu-ox/include/qemu/hbitmap.h:
+
+/home/lf/qemu-ox/include/qemu/bitops.h:
+
+/home/lf/qemu-ox/include/block/snapshot.h:
+
+/home/lf/qemu-ox/include/sysemu/blockdev.h:
+
+/home/lf/qemu-ox/include/trace.h:
+
+trace/generated-tracers.h:
+
+trace/control.h:
+
+trace/generated-events.h:
+
+trace/event-internal.h:
+
+trace/control-internal.h:
+
+/home/lf/qemu-ox/include/qom/cpu.h:
+
+/home/lf/qemu-ox/include/disas/bfd.h:
+
+/home/lf/qemu-ox/include/qemu/log.h:
+
+/home/lf/qemu-ox/include/sysemu/dma.h:
+
+/home/lf/qemu-ox/include/exec/address-spaces.h:
+
+/home/lf/qemu-ox/include/qemu/cutils.h:
