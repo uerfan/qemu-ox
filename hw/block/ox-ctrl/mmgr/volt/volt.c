b5ae5b75b46b8f194f954e95e394301c0ff878d9
李飞 <844186956@qq.com>
2018-05-30 21:10:17 +0800

    wd

--- hw/block/ox-ctrl/mmgr/volt/volt.c
+++ hw/block/ox-ctrl/mmgr/volt/volt.c
@@ -459,7 +459,7 @@ static int volt_process_io (struct nvm_mmgr_io_cmd *cmd)
 			sector_oob = sector_data + volt_mmgr.geometry->sec_size;
             if(core.debug){
                 //printf("[DEBUG] sizeof sector_data[10] = %ld \n",sizeof(sector_data[10]));
-                sector_data[10] = (sector_data[10] & 0xF0) | ((~(sector_data[10] & 0x0F)) & 0x0F);
+               // sector_data[10] = (sector_data[10] & 0xF0) | ((~(sector_data[10] & 0x0F)) & 0x0F);
             }
 			decode_ret = decode_bch(bch,sector_data,K_SIZE,sector_oob,NULL,NULL,errloc);
 			if(core.debug){
