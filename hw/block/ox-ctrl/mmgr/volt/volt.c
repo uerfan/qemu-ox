d1e0fde239709f3a549e00731202e86725c2be45
李飞 <844186956@qq.com>
2018-05-30 20:48:42 +0800

    wd

--- hw/block/ox-ctrl/mmgr/volt/volt.c
+++ hw/block/ox-ctrl/mmgr/volt/volt.c
@@ -456,13 +456,13 @@ static int volt_process_io (struct nvm_mmgr_io_cmd *cmd)
 
 		#ifdef USE_ECC
 			sector_data = dma->virt_addr;
-			sector_oob = dma->virt_addr + volt_mmgr.geometry->pg_size;
+			sector_oob = sector_data + volt_mmgr.geometry->pg_size;
             if(core.debug){
                 sector_data[10] = ~sector_data[10];
             }
 			decode_ret = decode_bch(bch,sector_data,K_SIZE,sector_oob,NULL,NULL,errloc);
 			if(core.debug){
-                printf("[DEBUG]decode_ret: %d.\n",decode_ret);
+                printf("[DEBUG] decode_ret: %d.\n",decode_ret);
             }
 			if(decode_ret< 0){
 				ret = 1;
@@ -479,15 +479,15 @@ static int volt_process_io (struct nvm_mmgr_io_cmd *cmd)
 			break;
         case MMGR_WRITE_PG:
 			dir = VOLT_DMA_WRITE;
-            volt_nand_dma (blk->pages[cmd->ppa.g.pg].data,dma->virt_addr, volt_mmgr.geometry->pg_size, dir);
-            
+            volt_nand_dma (blk->pages[cmd->ppa.g.pg].data,dma->virt_addr,volt_mmgr.geometry->pg_size, dir);
+
 		#ifdef USE_ECC
 			sector_data = blk->pages[cmd->ppa.g.pg].data;
-			sector_oob = blk->pages[cmd->ppa.g.pg].data+volt_mmgr.geometry->pg_size;
+			sector_oob = sector_data + volt_mmgr.geometry->pg_size;
             memset(sector_oob,0,OOB_ECC_LEN);
 			encode_bch(bch, sector_data, K_SIZE, sector_oob);
             if(core.debug){
-                printf("[DEBUG]encode_bch.\n");
+                printf("[DEBUG] encode_bch.\n");
             }
 		#endif
             
