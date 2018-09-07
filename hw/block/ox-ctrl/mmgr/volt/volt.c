33991740480b3597763fd4955779e788580cca8c
李飞 <844186956@qq.com>
2018-09-07 14:35:01 +0800

    wd

--- hw/block/ox-ctrl/mmgr/volt/volt.c
+++ hw/block/ox-ctrl/mmgr/volt/volt.c
@@ -427,7 +427,7 @@ static void volt_nand_dma (void *paddr, void *buf, size_t sz, uint8_t dir)
 				printf("\n");
 			}*/
 			printf("[DEBUG]: write size=%ld \n",sz);
-			printf("[DEBUG]: write data %.*s \n",sz>10:10?sz,(char*)buf);
+			printf("[DEBUG]: write data %.*s \n",(sz>10):10?sz,(char*)buf);
             memcpy(paddr, buf, sz);
             break;
     }
@@ -548,7 +548,9 @@ static void volt_execute_io (struct ox_mq_entry *req)
 				printf("[DEBUG] MMGR_WRITE_PG\n");
 				break;
 			case MMGR_WRITE_PG:
-				printf("[DEBUG] MMGR_READ_PG\n")
+				printf("[DEBUG] MMGR_READ_PG\n");
+				break;
+			default:
 		}
 	}
     ret = volt_process_io(cmd);
