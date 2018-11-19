af60baac158f8642c80b854b5ab984ad4416a362
李飞 <844186956@qq.com>
2018-11-19 20:15:03 +0800

    wd

--- hw/block/ox-ctrl/mmgr/volt/volt.c
+++ hw/block/ox-ctrl/mmgr/volt/volt.c
@@ -455,7 +455,6 @@ static int volt_process_io (struct nvm_mmgr_io_cmd *cmd)
     blk = volt_get_block(cmd->ppa);
 	dma->status = 1;
 	int rate=10;
-	int posi = 0;
 	
 	//printf("[DEBUG][volt process io]: vlot_page_sz:%d,vlot_oob_sz:%d,sec_per_page: %d\n",volt_mmgr.geometry->pg_size,volt_mmgr.geometry->sec_oob_sz,volt_mmgr.geometry->sec_per_pg);
 
@@ -466,7 +465,7 @@ static int volt_process_io (struct nvm_mmgr_io_cmd *cmd)
 			if(core.debug){
 				printf("[DEBUG][MMGR_READ_ECC_CTL]: %d\n",ECC_CTL);
 			}
-			posi=(int)(100*rand()/(RAND_MAX+1.0))
+			int posi = (int)(100*rand()/(RAND_MAX+1.0))
 			if(ECC_CTL==MMGR_ECC_ON && (rand()%100 < rate)){
 				ret = 1;
 				dma->status = 0;
