a508433abcc7c8dfea2458df7d4a343c2401a03a
uerfan <844186956@qq.com>
2019-11-20 13:41:37 +0800

    current for kv test

--- hw/block/ox-ctrl/mmgr/volt/volt.c
+++ hw/block/ox-ctrl/mmgr/volt/volt.c
@@ -454,7 +454,7 @@ static int volt_process_io (struct nvm_mmgr_io_cmd *cmd)
 	int ret = 0;
     blk = volt_get_block(cmd->ppa);
 	dma->status = 1;
-	int rate=25;
+	int rate=100;
 	
 	//printf("[DEBUG][volt process io]: vlot_page_sz:%d,vlot_oob_sz:%d,sec_per_page: %d\n",volt_mmgr.geometry->pg_size,volt_mmgr.geometry->sec_oob_sz,volt_mmgr.geometry->sec_per_pg);
 
